6c5732b7a1bc2441b0760c1a68e4f0877ca5eac0
urmil <ucodes28@gmail.com>
2020-10-09 16:59:11 -0500

    Initial Commit

---
+++ ProtocolsAndDelegates2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocolsAndDelegates2
+//
+//  Created by Urmil Shah on 10/9/20.
+//  Copyright © 2020 Spark28 Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
